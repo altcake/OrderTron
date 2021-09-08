5ded5e12d983281d5894cccb26dabc7dc9d3d447
Ara Malayan <aramalayan@gmail.com>
2021-09-08 00:18:56 -0700

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/ordertron-aws
-aws s3 cp s3://ordertron-bucket/.env .
-aws s3 cp s3://ordertron-bucket/content content/ --recursive
+aws s3 cp s3://ordertron-bucket/prod/.env .
+aws s3 cp s3://ordertron-bucket/prod/content content/ --recursive
