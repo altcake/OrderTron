692bd9c989afddcffdeab12dd619538c08ffc24c
Ara Malayan <aramalayan@gmail.com>
2021-09-20 02:08:35 -0700

    Update help function

--- src/commands/help.js
+++ src/commands/help.js
@@ -9,9 +9,9 @@ module.exports = {
             .setTitle('OrderTron Directives')
             .addFields(
                 { name: '~enforce', value: 'Use this to lay down the law.', inline: true },
-                { name: '~enka', value: 'Recommends some classy shit.', inline: true },
+                { name: '~enka', value: 'Recommends some classy music.', inline: true },
                 { name: '~training', value: '<NOT FINISHED> Obtain a training manual.'},
-                { name: '~XdX', value: 'Roll some dice!  This command can also be chained Eg. ~d20 2d10'},
+                { name: '~XdX or ~dX', value: 'Roll some dice!  This command can also be chained Eg. ~d20 2d10'},
             )
         message.member.send({ embeds: [print_help] })
     }
