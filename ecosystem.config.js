38c3a147dac848d5da70d0f36710ed12683359fa
Ara Malayan <aramalayan@gmail.com>
2021-09-20 01:30:52 -0700

    Fix improper working directory path for PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ordertron-aws',
-      cwd: '~/ordertron-aws',
+      cwd: '/home/ubuntu/ordertron-aws',
       script: './src/main.js',
       env: {
         NODE_ENV: 'prod',
