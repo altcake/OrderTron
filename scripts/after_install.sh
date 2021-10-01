4a51bcd238c307a9bdbbea12fa1442b69d6b9a1d
Ara Malayan <aramalayan@gmail.com>
2021-10-01 01:20:03 -0700

    Update after_install.sh to grant ubuntu user ownership over content folder

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,3 +2,4 @@
 cd ~/ordertron-aws
 sudo aws s3 cp s3://ordertron-bucket/prod/.env .
 sudo aws s3 cp s3://ordertron-bucket/prod/content content/ --recursive
+sudo chown -R ubuntu:ubuntu *
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudp apt install -y ruby
 sudo apt install -y wget
-# sudo apt install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+sudo apt install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 sudo apt install -y awscli
 
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
