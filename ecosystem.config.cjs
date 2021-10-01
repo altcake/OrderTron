da744228a231df6c6ffe3abc713cbc940ecb59bc
Ara Malayan <aramalayan@gmail.com>
2021-09-30 19:26:16 -0700

    Change ecosystem.config to work within ES6 project

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ordertron-aws',
+      cwd: '/home/ubuntu/ordertron-aws',
+      script: './src/main.js',
+      env: {
+        NODE_ENV: 'prod'
+      }
+    }
+  ]
+}
