ef5aeebc56edb8d99f7ed2850555ce6cc2c4e0f6
Ara Malayan <aramalayan@gmail.com>
2021-09-30 19:05:39 -0700

    2.5.0: convert ecosystem.config.js to ES6, fix AWS deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,10 @@
-module.exports = {
-  apps: [
-    {
-      name: 'ordertron-aws',
-      cwd: '/home/ubuntu/ordertron-aws',
-      script: './src/main.js',
-      env: {
-        NODE_ENV: 'prod',
-      },
-    },
-  ],
-}
+export const apps = [
+  {
+    name: 'ordertron-aws',
+    cwd: '/home/ubuntu/ordertron-aws',
+    script: './src/main.js',
+    env: {
+      NODE_ENV: 'prod'
+    }
+  }
+]
