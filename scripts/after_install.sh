499c3e81b526003ff0683f8611f9c11238695604
Ara Malayan <aramalayan@gmail.com>
2022-08-23 17:04:47 -0700

    2.8.4: Update support scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,3 +7,4 @@ sudo aws s3 cp s3://ordertron-bucket/prod/content content/ --recursive
 # Backup word data file from server to S3
 sudo aws s3 cp content/word_data/ s3://ordertron-bucket/prod/backup/word_data --recursive
 sudo chown -R ubuntu:ubuntu *
+chmod +x scripts/maintenance.sh
