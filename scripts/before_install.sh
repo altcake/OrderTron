3f37202222f1bff7439fc553aea766a0a3c15911
Ara Malayan <aramalayan@gmail.com>
2022-08-30 14:09:44 -0700

    2.9.1: Remove unnecessary packages from before_install, update vulnerable packages

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install required libraries
-sudo apt install -y ruby wget awscli build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+sudo apt install -y ruby wget awscli build-essential
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo npm install pm2 -g
